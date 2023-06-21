8904d9d5fcb256ff2d5a4affbb71b44f2bc4ebdb
ali kazemaini <kazemaini@kashanplus.ir>
2023-06-21 11:30:13 +0430

    add new styles in sidebar styles

---
+++ src/styles/sidebar/sidebarItem.style.js
@@ -0,0 +1,43 @@
+import {styled} from "@mui/material/styles";
+import {ListItemButton, ListItemIcon} from "@mui/material";
+
+export const StyledItem = styled((props) => <ListItemButton disableGutters {...props} />)(({ theme }) => ({
+    ...theme.typography.body2,
+    height: 48,
+    position: 'relative',
+    textTransform: 'capitalize',
+    color: theme.palette.text.secondary,
+    borderRadius: theme.shape.borderRadius,
+}));
+
+export const StyledItemIcon = styled(ListItemIcon)({
+    width: 35,
+    height: 35,
+    color: 'inherit',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+});
+
+export const activeItemStyle = {
+    '&.active': {
+        color: 'primary.main',
+        bgcolor: 'action.selected',
+        fontWeight: 'fontWeightBold',
+        boxShadow: (theme) => theme.palette.mode !== "dark" ?
+            "4px 4px 8px #c8c8c8,-4px -4px 8px #ffffff" :
+            "4px 4px 8px #0b0b0b,-4px -4px 8px #0f0f0f",
+
+    },
+}
+
+export const collapseStyle = {
+    px:2,
+    py:1,
+    color: 'primary.main',
+    borderRadius : (theme) => theme.shape.borderRadius,
+    boxShadow: (theme) => theme.palette.mode !== "dark" ?
+        "1px 1px 2px #c8c8c8,-1px -1px 2px #ffffff" :
+        "1px 1px 2px #303030,-1px -1px 2px #404040",
+
+}
